a60fb868f8de6fae9a02a3cf092dc1b4a079a53c
aidiaoemami <aidiaoemami@gmail.com>
2019-10-23 06:33:47 +0700

    Initial commit

---
+++ app/src/main/java/com/aidiaoemami/hajicerdas/AplikasiActivity.java
@@ -0,0 +1,13 @@
+package com.aidiaoemami.hajicerdas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AplikasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplikasi);
+    }
+}
---
+++ app/src/main/java/com/aidiaoemami/hajicerdas/ForgetPassActivity.java
@@ -0,0 +1,13 @@
+package com.aidiaoemami.hajicerdas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgetPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pass);
+    }
+}
